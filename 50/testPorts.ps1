5c211a5bd61bec83777348beac88353482f85bd0
Antonio Salazar <antsalgra@hotmail.com>
2022-09-11 04:58:08 -0700

    Actualizado

--- 50/testPorts.ps1
+++ 50/testPorts.ps1
@@ -9,7 +9,7 @@ function Test-Port {
         [Parameter(ValueFromPipeline = $true, HelpMessage = 'Se le puede poner :Port')]
         [String[]]$ComputerName,
 
-        [Parameter(HelpMessage = 'Seráááááá ignorado si el puerto ya se proporciona en el parámetro ComputerName')]
+        [Parameter(HelpMessage = 'Ser�����ignorado si el puerto ya se proporciona en el parámetro ComputerName')]
         [Int]$Port = 5985,
 
         [Parameter(HelpMessage = 'Timeout en milisegundos.')]
