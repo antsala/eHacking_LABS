01931c8dd73efaf63df1c9f48f3a817a232ca93d
Antonio Salazar Graván <antsalgra@hotmail.com>
2022-09-11 13:10:25 +0200

    Contenido actualizado

--- 50/testPorts.ps1
+++ 50/testPorts.ps1
@@ -1,3 +1,3 @@
 $VICTIM_IP="192.168.20.13" # Es Metasploitable3-ubu1404
 
-1.. 1024 | ForEach-Object{Write-Output((new-object Net.Sockets.TcpClient).Connect($VICTIM_IP,$_)) "El puerto $_ está abierto"} 2>$null
+1.. 1024 | ForEach-Object{Write-Output((new-object Net.Sockets.TcpClient).Connect($VICTIM_IP,$_)) "Puerto $_ abierto"}
